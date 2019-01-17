4440374df6e3547c2179639eba4e88c66151f905
Stacey Chin <sschin@ucsd.edu>
2019-01-17 10:49:40 -0800

    added Javascript

---
+++ static/js/introHCI.js
@@ -0,0 +1,14 @@
+function initializePage() {
+	$("#testjs").click(function(e){
+		$('.jumbotron h1').text("Javascript is connected");
+	});
+	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) { 
+	e.preventDefault();
+ 	$(this).css("background-color", "#7fff00");
+}
+$(document).ready(function() {
+	initializePage();
+})
